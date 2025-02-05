e6a936e108bcfa847db5620747198a4b7df9303d
Helion12 <benrouhasalma5@gmail.com>
2025-02-05 15:30:49 +0100

    Add files via upload
    Signed-off-by: Helion12 <benrouhasalma5@gmail.com>

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("chatbot-toggle").addEventListener("click", function() {
+    let chatWindow = document.getElementById("chatbot-window");
+    chatWindow.classList.toggle("hidden");
+});
