965ff916bbbf9e458fc122d5f3c2f6df2d675f25
Helion12 <benrouhasalma5@gmail.com>
2025-02-07 13:16:01 +0100

    Delete script.js
    Signed-off-by: Helion12 <benrouhasalma5@gmail.com>

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("chatbot-toggle").addEventListener("click", function() {
-    let chatWindow = document.getElementById("chatbot-window");
-    chatWindow.classList.toggle("hidden");
-});
